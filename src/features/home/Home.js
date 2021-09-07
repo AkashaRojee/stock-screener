9d0dbfb017b3b6c39d12dd3f4621f5d0e6157037
Akasha <Akasha.Rojee@gmail.com>
2021-09-07 15:26:53 +0400

    Set up market slice and GET Symbols List

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,40 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import HighlightCard from 'common/components/HighlightCard/HighlightCard';
 import SectionTitle from 'common/components/SectionTitle';
+import { getSymbolsList } from './marketSlice';
 import SymbolCard from './SymbolCard';
 import styles from './Home.module.scss';
 
-const Home = () => (
-  <>
-    <HighlightCard
-      image="AAPL"
-      name="HIGHEST-PRICED COMPANY NAME"
-      metric="$1000"
-    />
-    <SectionTitle title="STATS BY COMPANY" />
-    <div className={styles.symbolCards}>
-      <SymbolCard
-        image="CMCSA"
-        name="Comcast Corporation"
-        metric="$16"
-      />
-      <SymbolCard
-        image="INTC"
-        name="Intel Corporation"
-        metric="$53"
-      />
-      <SymbolCard
+const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getSymbolsList());
+  }, []);
+
+  return (
+    <>
+      <HighlightCard
         image="AAPL"
-        name="Apple"
+        name="HIGHEST-PRICED COMPANY NAME"
         metric="$1000"
       />
-      <SymbolCard
-        image="MSFT"
-        name="Microsoft"
-        metric="$89"
-      />
-    </div>
-  </>
-);
+      <SectionTitle title="STATS BY COMPANY" />
+      <div className={styles.symbolCards}>
+        <SymbolCard
+          image="CMCSA"
+          name="Comcast Corporation"
+          metric="$16"
+        />
+        <SymbolCard
+          image="INTC"
+          name="Intel Corporation"
+          metric="$53"
+        />
+        <SymbolCard
+          image="AAPL"
+          name="Apple"
+          metric="$1000"
+        />
+        <SymbolCard
+          image="MSFT"
+          name="Microsoft"
+          metric="$89"
+        />
+      </div>
+    </>
+  );
+};
 
 export default Home;
--- src/features/home/marketSlice.js
+++ src/features/home/marketSlice.js
@@ -1,12 +1,27 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const getSymbolsList = createAsyncThunk('/stock/list/get', async () => {
+  const { data } = await axios.get('https://financialmodelingprep.com/api/v3/stock/list?apikey=7c1ec8628463b42568e264b7e2fac67d');
+  return data;
+});
 
 export const marketSlice = createSlice({
   name: 'markets',
-  initialState: [],
+  initialState: {},
   reducers: {
-
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getSymbolsList.fulfilled, (state, action) => {
+      action.payload.forEach((symbolObject) => {
+        const { exchange } = symbolObject;
+        state[exchange] = [];
+      });
+    });
   },
 
 });
+
+// export const { add } = marketSlice.actions;
 
 export default marketSlice.reducer;
