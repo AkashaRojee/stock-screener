75fd3ead057e33395e9ecd6ffc29777767eccce1
Akasha <Akasha.Rojee@gmail.com>
2021-09-07 14:40:40 +0400

    Create base link between SymbolCard and Details

--- src/features/home/SymbolCard.js
+++ src/features/home/SymbolCard.js
@@ -1,16 +1,17 @@
 /* eslint-disable react/prop-types */
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './SymbolCard.module.scss';
 
 const SymbolCard = ({ image, name, metric }) => (
-  <div className={styles.symbolCard}>
+  <Link to="/details" className={styles.symbolCard}>
     <div className={styles.image}>
       <img src={`https://financialmodelingprep.com/image-stock/${image}.jpg`} alt={name} />
     </div>
     <div>{ name }</div>
     <div>{ metric }</div>
-  </div>
+  </Link>
 );
 
 export default SymbolCard;
