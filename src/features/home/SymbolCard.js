c4944377fbd169f03455c063740964e312f4ea67
Akasha <Akasha.Rojee@gmail.com>
2021-09-08 01:36:46 +0400

    Set fallback image for company logo

--- src/features/home/SymbolCard.js
+++ src/features/home/SymbolCard.js
@@ -4,18 +4,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './SymbolCard.module.scss';
+import fallbackImage from 'assets/image-not-found.png';
 
-const SymbolCard = ({ image, name, metric }) => (
-  <Link to={`/symbols/${image}`} className={styles.symbolCard}>
-    <div className={styles.image}>
-      <img
-        src={`https://financialmodelingprep.com/image-stock/${image}.jpg`}
-        alt={name}
+const SymbolCard = ({ image, name, metric }) => {
+
+  return (
+    <Link to={`/symbols/${image}`} className={styles.symbolCard}>
+      <div className={styles.image}>
+        <img
+          src={`https://financialmodelingprep.com/image-stock/${image}.jpg`}
+          alt={name}
+          onError={(e) => e.target.src=fallbackImage}
         />
-    </div>
-    <div>{ name }</div>
-    <div>{ metric }</div>
-  </Link>
-);
+      </div>
+      <div>{ name }</div>
+      <div>{ metric }</div>
+    </Link>
+  );
+};
 
 export default SymbolCard;
