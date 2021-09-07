1332ab29c1c91ff608702a92e1211ae5fa6b03b2
Akasha <Akasha.Rojee@gmail.com>
2021-09-07 14:49:09 +0400

    Structure DataRows

--- src/features/details/Symbol.js
+++ src/features/details/Symbol.js
@@ -2,6 +2,7 @@ import HighlightCard from 'common/components/HighlightCard/HighlightCard';
 import SectionTitle from 'common/components/SectionTitle';
 import React from 'react';
 import DataRow from './DataRow';
+import styles from './Symbol.module.scss';
 
 const Symbol = () => (
   <>
@@ -11,10 +12,24 @@ const Symbol = () => (
       metric="$150"
     />
     <SectionTitle title="QUOTE BREAKDOWN" />
-    <DataRow
-      item="Price"
-      metric="100"
-    />
+    <div className={styles.dataRows}>
+      <DataRow
+        item="Price"
+        metric="$100"
+      />
+      <DataRow
+        item="Open"
+        metric="$102.30"
+      />
+      <DataRow
+        item="Previous close"
+        metric="$109.30"
+      />
+      <DataRow
+        item="Volume"
+        metric="828190"
+      />
+    </div>
   </>
 );
 
