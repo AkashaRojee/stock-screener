aa2ca5c8939f1cd34322f6061cdaebbc6451a486
Akasha <Akasha.Rojee@gmail.com>
2021-09-07 19:27:35 +0400

    Dynamically generate SymbolCards for each symbol

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,5 +1,7 @@
+/* eslint-disable */
+
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import HighlightCard from 'common/components/HighlightCard/HighlightCard';
 import SectionTitle from 'common/components/SectionTitle';
 import { getSymbolsList } from './marketSlice';
@@ -7,11 +9,14 @@ import SymbolCard from './SymbolCard';
 import styles from './Home.module.scss';
 
 const Home = () => {
+  const markets = useSelector((state) => state.market);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getSymbolsList());
+    if (Object.keys(markets).length === 0) dispatch(getSymbolsList());
   }, []);
+
+  const symbols = markets['New York Stock Exchange'];
 
   return (
     <>
@@ -20,28 +25,22 @@ const Home = () => {
         name="HIGHEST-PRICED COMPANY NAME"
         metric="$1000"
       />
+      
       <SectionTitle title="STATS BY COMPANY" />
+        
       <div className={styles.symbolCards}>
-        <SymbolCard
-          image="CMCSA"
-          name="Comcast Corporation"
-          metric="$16"
-        />
-        <SymbolCard
-          image="INTC"
-          name="Intel Corporation"
-          metric="$53"
-        />
-        <SymbolCard
-          image="AAPL"
-          name="Apple"
-          metric="$1000"
-        />
-        <SymbolCard
-          image="MSFT"
-          name="Microsoft"
-          metric="$89"
-        />
+
+        {symbols && Object.entries(symbols).map(([symbol, { name, price }]) => (
+          
+          <SymbolCard
+            key={symbol}
+            image={symbol}
+            name={name}
+            metric={price}
+          />
+
+        ))}
+
       </div>
     </>
   );
