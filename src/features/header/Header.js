0dd1dd4f6a3cb4249a288976ab873a241113af0d
Akasha <Akasha.Rojee@gmail.com>
2021-09-08 09:01:36 +0400

    Change header text according to route

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,10 +1,22 @@
+/* eslint-disable */
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import MicIcon from '@material-ui/icons/Mic';
+import SettingsIcon from '@material-ui/icons/Settings';
+import styles from './Header.module.scss';
 
-const Header = () => (
-  <>
-    <Link to="/">&lt;</Link>
-  </>
-);
+const Header = () => {
+  const { pathname } = useLocation(); 
+
+  return (
+    <div className={styles.header}>
+      <Link to="/"><ArrowBackIosIcon /></Link>
+      <span>{pathname === '/' ? 'Highest value' : 'Symbol value'}</span>
+      <MicIcon className={styles.mic} />
+      <SettingsIcon />
+    </div>
+  );
+};
 
 export default Header;
