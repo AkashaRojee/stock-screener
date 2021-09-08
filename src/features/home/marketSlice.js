08ca2e758f5d2db11e89ea08e78040a25ec95389
Akasha <Akasha.Rojee@gmail.com>
2021-09-08 19:52:33 +0400

    Update build for deploy

--- src/features/home/marketSlice.js
+++ src/features/home/marketSlice.js
@@ -4,7 +4,7 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const getSymbolsList = createAsyncThunk('/stock/list/get', async () => {
-  const { data } = await axios.get('https://financialmodelingprep.com/api/v3/stock/list?apikey=7c1ec8628463b42568e264b7e2fac67d');
+  const { data } = await axios.get('https://financialmodelingprep.com/api/v3/stock/list?apikey=94d238a41487fee05de69bf52aa5deec');
   return data;
 });
 
