f0c528a187029b37e3578a54ef5eb1ca4a3b1acf
Akasha <Akasha.Rojee@gmail.com>
2021-09-08 08:36:55 +0400

    Disable serializable check middleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,13 @@
+/* eslint-disable */
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './rootReducer';
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export default store;
