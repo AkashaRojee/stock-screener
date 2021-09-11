e9a854509dd2cd1bb27705f9c100c4b44baa2436
Akasha <Akasha.Rojee@gmail.com>
2021-09-11 09:37:48 +0400

    Set up testing

---
+++ src/test-utils.js
@@ -0,0 +1,30 @@
+// @ts-nocheck
+import React from 'react';
+import { render as rtlRender } from '@testing-library/react';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import marketReducer from 'features/home/marketSlice';
+import symbolsReducer from 'features/details/symbolsSlice';
+
+function render(
+  ui,
+  {
+    preloadedState,
+    store = configureStore(
+      {
+        reducer: { market: marketReducer, symbols: symbolsReducer },
+        preloadedState,
+      },
+    ),
+    ...renderOptions
+  } = {},
+) {
+  function Wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+export * from '@testing-library/react';
+
+export { render };
